a0ff77b103af9a12cef2f5eed77a397aa2390a5f
ohdj <71207981+ohdj@users.noreply.github.com>
2024-07-01 20:13:35 +0800

    #8 信息组件BaseComponent
    https://www.bilibili.com/video/BV1ga411s7MG

---
+++ TextComponentPlugin/src/main/java/com/ohdj/textcomponentplugin/executor/PrintMessage.java
@@ -0,0 +1,72 @@
+package com.ohdj.textcomponentplugin.executor;
+
+import com.ohdj.textcomponentplugin.TextComponentPlugin;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.text.MessageFormat;
+import java.util.Objects;
+
+public class PrintMessage implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            if (Objects.nonNull(args) && args.length > 0) {
+                String arg = args[0];
+                switch (arg) {
+                    case "1":
+                        fun1(player);
+                        break;
+                    case "2":
+                        fun2(player);
+                        break;
+                }
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private void fun1(Player player) {
+        // 用法一：ChatColor拼接
+        String message1 = ChatColor.GOLD + "你好 " + ChatColor.BOLD + ChatColor.RED + "世界";                        // 转义后实际得到的文本    §6你好 §c世界
+        // 等同于
+        String message11 = ChatColor.GOLD.toString() + "你好 " + ChatColor.BOLD.toString() + ChatColor.RED.toString() + "世界";
+        player.sendMessage(message1);
+
+        // 用法二：ChatColor.translateAlternateColorCodes(字符,字符串)转换颜色代码
+        String message2 = ChatColor.translateAlternateColorCodes('&', "&6你好 &l&c世界");   // 转义后实际得到的文本    §6你好 §c世界
+        player.sendMessage(message2);
+    }
+
+    /**
+     * 完全自定义的色彩 <br><br>
+     * <p>
+     * {@link ChatColor}枚举所提供的色彩存在一定的限制<br>
+     * 如果需要自定义色彩，我们可以通过{@link net.md_5.bungee.api.ChatColor#of(String)}传入十六进制颜色码来细分色彩
+     */
+    private void fun2(Player player) {
+        net.md_5.bungee.api.ChatColor color1 = net.md_5.bungee.api.ChatColor.of("#FCF3CF"); // 十六进制的颜色码
+        net.md_5.bungee.api.ChatColor color2 = net.md_5.bungee.api.ChatColor.of("#F9E79F"); // 十六进制的颜色码
+        net.md_5.bungee.api.ChatColor color3 = net.md_5.bungee.api.ChatColor.of("#F7DC6F"); // ...
+        net.md_5.bungee.api.ChatColor color4 = net.md_5.bungee.api.ChatColor.of("#F4D03F");
+        net.md_5.bungee.api.ChatColor color5 = net.md_5.bungee.api.ChatColor.of("#F1C40F");
+        net.md_5.bungee.api.ChatColor color6 = net.md_5.bungee.api.ChatColor.of("#D4AC0D");
+
+        // 发送 纯文本            这里也是用了toString()特性
+        String pureText = MessageFormat.format("{0}大{1}家{2}好{3}啊{4}欢{5}迎", color1, color2, color3, color4, color5, color6);
+        // pureText.equals(message) == true
+        String message = color1 + "大" + color2 + "家" + color3 + "好" + color4 + "啊" + color5 + "欢" + color6 + "迎";
+        player.sendMessage(pureText + ChatColor.translateAlternateColorCodes('&', "       &f sent in pure text form!"));
+
+
+        // color1实际对应的颜色代码
+        TextComponentPlugin.getInstance().getLogger().info("color1实际对应的颜色代码" + color1);
+    }
+}
---
+++ TextComponentPlugin/src/main/java/com/ohdj/textcomponentplugin/executor/PrintTextComponent.java
@@ -0,0 +1,84 @@
+package com.ohdj.textcomponentplugin.executor;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * @author ohdj
+ * @date 2022/7/1 15:46
+ */
+public class PrintTextComponent implements CommandExecutor {
+
+    /**
+     * {@link TextComponent} 是 {@link BaseComponent}抽象类的一个子类，在继承信息组件所必有的 "文本"、"字体色彩"、"字体样式"<br>
+     * 的同时，TextComponent还具备了 鼠标的点击以及悬浮事件。</br>
+     * 最终信息组件需要使用 {@link Player#spigot()} 来发送，这是发送组件与发送字符串常量的区别所在。
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            // 步骤一：创建信息组件，一个用于显示在聊天框，另一个显示在物品栏上方
+            TextComponent chatMessage = getTextComponent();
+            BaseComponent[] actionBarMessage = getComponents(player);
+
+            // 步骤二：获取玩家的Spigot对象
+            Player.Spigot spigot = player.spigot();
+
+            // 步骤三：通过Spigot对象将信息组件发送给玩家 [可选位置：聊天框、物品栏上方、系统级提示]
+            // spigot.sendMessage(textComponent);                            // 默认的发送位置为ChatMessageType.CHAT，也就是在聊天框里显示
+            spigot.sendMessage(ChatMessageType.CHAT, chatMessage);        // 还可以将位置改为ChatMessageType.ACTION_BAR，此时就能够显示在经验条、血量条上方的区域
+            spigot.sendMessage(ChatMessageType.ACTION_BAR, actionBarMessage);  // 还可以将位置改为ChatMessageType.SYSTEM，实际的效果与ChatMessageType.CHAT一样，区别在于这是系统级别的消息
+
+            return true;
+        }
+        return false;
+    }
+
+    private TextComponent getTextComponent() {
+        TextComponent text1 = new TextComponent("你好");
+        text1.setColor(net.md_5.bungee.api.ChatColor.RED);              // 设置颜色
+        text1.setBold(true);                                            // 设置粗体
+        // text1.setItalic(true);                                        // 设置斜体
+        // text1.setObfuscated(true);                                    // 设置模糊（效果是不停变换的乱码）
+        // ...
+
+        // 添加点击事件       其实本质上是让玩家说了一句话，不信你可以把这里"/help TextComponentPlugin"中的斜杠去掉
+        text1.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/help TextComponentPlugin"));
+        // 添加鼠标悬浮事件
+        text1.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("这是一个鼠标悬浮提示")));
+
+        // 拼接下一段信息
+        TextComponent text2 = new TextComponent("世界");
+        text2.setBold(false);
+        text2.setObfuscated(true);
+        text2.setColor(net.md_5.bungee.api.ChatColor.GREEN);
+
+        // 组件的拼接会自动继承上一个组件的颜色、粗体、斜体、鼠标事件等属性
+        text1.addExtra(text2);                                         // 拼接下一段信息
+
+        // Spigot内部类的sendMessage()支持多参传入
+        // player.spigot().sendMessage(text1, text2); 不想拼接的话，可以这样写，记得不要用text1.addExtra(text2);
+        return text1;
+    }
+
+    private BaseComponent[] getComponents(Player player) {
+        // 由于组件构造器的特殊性，必须.append()在前，然后在指定[颜色、字体、粗体、斜体、模糊]等样式
+        return new ComponentBuilder()
+                .append("当前所在位置：").color(ChatColor.RED).bold(true)
+                .append(player.getLocation().getBlockX() + " ").color(ChatColor.GOLD).bold(false)
+                .append(player.getLocation().getBlockY() + " ").color(ChatColor.YELLOW)
+                .append(player.getLocation().getBlockZ() + " ").color(ChatColor.GREEN)
+                .append("  ").color(ChatColor.BLUE)
+                .append("延迟：" + player.getPing()).color(ChatColor.DARK_PURPLE).bold(true)
+                .append("...").color(ChatColor.LIGHT_PURPLE).italic(true)
+                .create();
+    }
+}
